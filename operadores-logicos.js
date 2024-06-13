02fd50913c52764c8658cfbd388eb8ec55c6cbc9
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-06-12 19:35:06 -0500

    testing operadores

---
+++ operadores-logicos.js
@@ -0,0 +1,16 @@
+//operadores logicos
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(b);
+
+//Operador logico (and)
+
+console.log(`${a} && ${b} -> ${a && b}`); // true && false -> false
+
+//Operador logico (or - ||)
+console.log(`${a} || ${b} -> ${a || b}`); // true || false -> false
+
+//Operador ! (nor or NO)
+console.log(`${a} -> ${!a}`);
---
+++ range.js
@@ -0,0 +1,8 @@
+//Ejemplo valor dentro del rango
+let min = 0, max = 5;
+
+let dato = 6; // valor de calcular si esta en el rango
+
+let insideRange = dato >= min && dato <= max;
+console.log('Valor dentro rango: ', insideRange);
+
